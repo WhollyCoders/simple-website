89e7ba34e085f94d0bcf8958df565dcffef2521d
Markus Gamble <markusgamble@Markuss-MacBook-Pro.local>
2017-08-12 10:35:47 -0400

    intinal commit

---
+++ contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Markus website</title>
+  </head>
+  <body>
+    <div class="container">
+  <a href="./index.php">home</a> | <a href="./about.php">about</a> | <a href="./contact.php">contact</a>
+      <h1>contact Markus</h1>
+    </div>
+  </body>
+</html>
