d7b839f5baa9a6bbb7ab437854cd0dd1759a2c7d
Michael L Parks <github@wmi-inc.com>
2017-08-12 20:45:11 -0400

    Updated Styles

--- index.php
+++ index.php
@@ -2,12 +2,45 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Markus website</title>
+    <title>Michael L Parks | Web Programmer</title>
+    <link rel="stylesheet" href="./css/main.css">
   </head>
   <body>
     <div class="container">
-      <a href="./index.php">home</a> | <a href="./about.php">about</a> | <a href="./contact.php">contact</a>
-      <h1>Markus home page</h1>
+      <header>
+        <div class="brand">
+          <div class="brand-wrapper">
+            <h1>Michael L Parks</h1>
+            <p>Full Stack Web Developer</p>
+          </div>
+        </div>
+        <div class="navigation">
+          <nav>
+            <ul>
+              <li><a class="active" href="./index.php">Home</a></li>
+              <li><a href="./about.php">About</a></li>
+              <li><a href="./contact.php">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <section id="mlp-banner">
+        <img src="./images/banner-home.jpg" alt="Home Banner">
+      </section>
+      <section id="mlp-content">
+        <div class="content-wrapper">
+          <img src="./images/profile.jpg" alt="Michael L Parks">
+          <h1>Michael L Parks</h1>
+          <p>Full-Stack Web Developer driven by an expectation of excellence,
+            motivated by the desire to help others, receives great satisfaction
+            from solving problems, relishes in the collaboration process and
+            values the experiences that result from pursuing higher learning.
+          </p>
+        </div>
+      </section>
+      <footer>
+        <p>&copy; 2017 Michael L Parks</p>
+      </footer>
     </div>
   </body>
 </html>
